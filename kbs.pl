efc60634f38bb8877fc8b03f1642c2098e93f125
Marco Baturan <marco.baturan@gmail.com>
2022-04-01 00:09:10 +0200

    knowledge base in prolog

---
+++ kbs.pl
@@ -0,0 +1,105 @@
+% Extracción de reglas para desparasitar canes.
+% Ectoparásitos y épocas.
+% NOROESTE
+pulgas:-
+  tiene_pulgas,
+  pregunta('¿Es perro?'),
+  pregunta('¿Estuvo en verano?'),
+  pregunta('¿Ha pasado por Galicia?');
+  pregunta('¿Ha pasado por Asturias?');
+  pregunta('¿Ha pasado por Cantabria?');
+  pregunta('¿Ha pasado por Pais Vasco?');
+  pregunta('¿Ha pasado por Navarra?');
+  pregunta('¿Ha pasado por La Rioja?');
+  pregunta('¿Ha pasado por Castilla y León?').
+  
+garrapatas:-
+  tiene_garrapatas,
+  pregunta('¿Es perro?'),
+  pregunta('¿Estuvo en otoño o primavera?'),
+  pregunta('¿Ha pasado por Galicia?');
+  pregunta('¿Ha pasado por Asturias?');
+  pregunta('¿Ha pasado por Cantabria?');
+  pregunta('¿Ha pasado por Pais Vasco?');
+  pregunta('¿Ha pasado por Navarra?');
+  pregunta('¿Ha pasado por La Rioja?');
+  pregunta('¿Ha pasado por Castilla y León?').
+  
+flebotomos:-
+  tiene_flebotomos,
+  pregunta('¿Es perro?'),
+  pregunta('¿Estuvo en Junio?');
+  pregunta('¿Estuvo en Julio?');
+  pregunta('¿Estuvo en Agosto?');
+  pregunta('¿Estuvo en Septiembre?');
+  pregunta('¿Estuvo en Octubre?'),
+  pregunta('¿Ha pasado por Galicia?');
+  pregunta('¿Ha pasado por Asturias?');
+  pregunta('¿Ha pasado por Cantabria?');
+  pregunta('¿Ha pasado por Pais Vasco?');
+  pregunta('¿Ha pasado por Navarra?');
+  pregunta('¿Ha pasado por La Rioja?');
+  pregunta('¿Ha pasado por Castilla y León?').
+  
+mosquitos:-
+  tiene_mosquitos,
+  pregunta('¿Es perro?'),
+  pregunta('¿Ha estado en zonas de agua estancada en torno al amanecer o anochecer?'),
+  pregunta('¿Estuvo en Junio?');
+  pregunta('¿Estuvo en Julio?');
+  pregunta('¿Estuvo en Agosto?');
+  pregunta('¿Estuvo en Septiembre?');
+  pregunta('¿Estuvo en Octubre?'),
+  pregunta('¿Ha pasado por Galicia?');
+  pregunta('¿Ha pasado por Asturias?');
+  pregunta('¿Ha pasado por Cantabria?');
+  pregunta('¿Ha pasado por Pais Vasco?');
+  pregunta('¿Ha pasado por Navarra?');
+  pregunta('¿Ha pasado por La Rioja?');
+  pregunta('¿Ha pasado por Castilla y León?').
+  
+% SURESTE
+pulgas:-
+  tiene_pulgas,
+  pregunta('¿Es perro?'),
+  pregunta('¿Estuvo en verano?'),
+  pregunta('¿Ha pasado por Cataluña?');
+  pregunta('¿Ha pasado por Comunidad Valenciana?');
+  pregunta('¿Ha pasado por Murcia?');
+  pregunta('¿Ha pasado por Andalucia?');
+  pregunta('¿Ha pasado por Islas Baleares?').
+  
+garrapatas:-
+  tiene_garrapatas,
+  pregunta('¿Es perro?'),
+  pregunta('¿Estuvo en Abril?');
+  pregunta('¿Estuvo en Mayo?');
+  pregunta('¿Estuvo en Junio?');
+  pregunta('¿Estuvo en Julio?');
+  pregunta('¿Estuvo en Agosto?');
+  pregunta('¿Estuvo en Septiembre?');
+  pregunta('¿Estuvo en Octubre?'), 
+  pregunta('¿Ha pasado por Cataluña?');
+  pregunta('¿Ha pasado por Comunidad Valenciana?');
+  pregunta('¿Ha pasado por Murcia?');
+  pregunta('¿Ha pasado por Andalucia?');
+  pregunta('¿Ha pasado por Islas Baleares?').
+  
+flebotomos:-
+  tiene_flebotomos,
+  pregunta('¿Es perro?'),
+  pregunta('¿Ha pasado por Cataluña?');
+  pregunta('¿Ha pasado por Comunidad Valenciana?');
+  pregunta('¿Ha pasado por Murcia?');
+  pregunta('¿Ha pasado por Andalucia?');
+  pregunta('¿Ha pasado por Islas Baleares?').
+  
+flebotomos:-
+  tiene_flebotomos,
+  pregunta('¿Es perro?'),
+  pregunta('¿Ha estado en zonas de agua estancada en torno al amanecer o anochecer?'),
+  pregunta('¿Ha pasado por Cataluña?');
+  pregunta('¿Ha pasado por Comunidad Valenciana?');
+  pregunta('¿Ha pasado por Murcia?');
+  pregunta('¿Ha pasado por Andalucia?');
+  pregunta('¿Ha pasado por Islas Baleares?').
